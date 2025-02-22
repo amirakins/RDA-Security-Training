9e8f825bc8a9156d8fc4f54aacf9cd5acd4f32ae
amirakins <70003484+amirakins@users.noreply.github.com>
2025-02-22 10:46:02 -0500

    fix typo "basic rifle"

--- rda-security-training/src/components/BookClass.jsx
+++ rda-security-training/src/components/BookClass.jsx
@@ -23,7 +23,7 @@ function BookClass() {
       <CourseCard
         link="/waiver"
         img={Ar15}
-        name="Basic Rifle MarksmanshipBasic Rifle Marksmanship"
+        name="Basic Rifle Marksmanship"
         desc="This course is structured to teach the essential skills and knowledge required to handle a rifle safely. 
         Participants will learn key concepts such as proper rifle handling, aiming techniques, shooting stances, and basic rifle maintenance."
         who="This course is ideal for individuals with little to no prior experience with rifles who wish to gain a solid understanding of how to use them effectively and safely."
